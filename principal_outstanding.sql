8a86556e4213f990921c2ca0f8413ffcc718b7b3
venmani-fsmk <138454275+venmani-fsmk@users.noreply.github.com>
2024-12-27 13:32:59 +0530

    Create principal_outstanding.sql

---
+++ principal_outstanding.sql
@@ -0,0 +1,6 @@
+select reference_date, loan_product_name, sum(principal_outstanding_lcl_amt) as metric,
+    count(distinct(cmd_ctr_borrower_id)) as unique_borrowers
+    from adm.transaction.loan_daily_outstanding_denorm_t
+    where loan_platform_country_code="MY"
+    AND LOAN_ORIGINATION_LCL_AMT!=0
+    group by reference_date, loan_product_name
