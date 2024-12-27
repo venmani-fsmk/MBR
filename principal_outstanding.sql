79902a63f83328d9cc858a80cdb8a8342460ca4c
venmani-fsmk <138454275+venmani-fsmk@users.noreply.github.com>
2024-12-27 13:34:41 +0530

    Update principal_outstanding.sql

--- principal_outstanding.sql
+++ principal_outstanding.sql
@@ -1,6 +1,7 @@
+-- Code for calculating principal outstanding on each date for MY Borrowers
 select reference_date, loan_product_name, sum(principal_outstanding_lcl_amt) as metric,
     count(distinct(cmd_ctr_borrower_id)) as unique_borrowers
     from adm.transaction.loan_daily_outstanding_denorm_t
-    where loan_platform_country_code="MY"
+    where loan_platform_country_code='MY'
     AND LOAN_ORIGINATION_LCL_AMT!=0
     group by reference_date, loan_product_name
