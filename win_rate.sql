19d9cb1ea61c2250f75a5c36b29467ca4ea7da59
venmani-fsmk <138454275+venmani-fsmk@users.noreply.github.com>
2025-01-02 09:00:42 +0530

    Create win_rate.sql

---
+++ win_rate.sql
@@ -0,0 +1,17 @@
+select DATE_TRUNC('MONTH',PIPEDRIVE_DEAL_CREATED_LCL_TS::date) AS REFERENCE_DATE, 
+    count(*) as total_deals, 
+    sum(case when HAVE_MADE_TO_S1_CONTACT_MADE_STAGE_FLAG=True then 1 else 0 end) as contact_made, 
+    sum(case when HAVE_MADE_TO_S2_LEAD_QUALIFIED_STAGE_FLAG=True then 1 else 0 end) as lead_qualified, 
+    sum(case when HAVE_MADE_TO_S3_APP_SUBMITTED_STAGE_FLAG=True then 1 else 0 end) as app_submitted, 
+    sum(case when HAVE_MADE_TO_S4_APP_APPROVED_STAGE_FLAG=True then 1 else 0 end) as app_approved, 
+    sum(case when have_made_to_final_won_stage_flag=True then 1 else 0 end) as total_won,
+    total_won*100/total_deals as metric,
+        TOTAL_WON*100/APP_APPROVED AS BRW_ACCEPTANCE,
+    APP_APPROVED*100/APP_SUBMITTED AS UND_ACCEPTANCE,
+    APP_SUBMITTED*100/LEAD_QUALIFIED AS SUBMISSION_RATE
+    from CDM.OPERATIONS.MY_PIPEDRIVE_DEAL_CONVERSION_T a
+    --where a.pipedrive_deal_pipeline_name='MY - Micro Pipeline'
+    --and a.PIPEDRIVE_DEAL_STATUS!='open'
+    WHERE a.pipedrive_deal_type='New to FS'
+    group by REFERENCE_DATE
+    order by REFERENCE_DATE desc;
